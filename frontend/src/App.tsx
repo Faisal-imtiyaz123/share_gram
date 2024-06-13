a4ab57a5980d55a8750a5c20d5c35ac9bbb633d8
Faisal-imtiyaz123 <ee23btech11019@iith.ac.in>
2024-06-13 06:45:46 +0530

    made App root route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,35 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import { trpc } from "./lib/trpc";
+import { httpBatchLink } from "@trpc/client";
+import { getAuthCookie } from "./lib/actions/auth/auth.actions";
+import { Outlet } from "react-router-dom";
+import ProtectedRoute from "./components/customAuth/ProtectedRoute";
 
 export default function App() {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: 'http://localhost:3000/trpc',
+          // You can pass any HTTP headers you wish here
+          async headers() {
+            return {
+            Authorization: getAuthCookie(),
+            };
+          },
+        }),
+      ],
+    }),
+  );
   return (
-    <div>App</div>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+      <ProtectedRoute>
+        <Outlet/>
+      </ProtectedRoute>
+      </QueryClientProvider>
+    </trpc.Provider>
   )
 }
