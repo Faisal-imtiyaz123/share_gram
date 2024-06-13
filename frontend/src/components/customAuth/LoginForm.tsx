84103ae5bda69f520ff9f1ef12c39254171665b9
Faisal-imtiyaz123 <ee23btech11019@iith.ac.in>
2024-06-13 06:18:32 +0530

    LoginForm made

---
+++ frontend/src/components/customAuth/LoginForm.tsx
@@ -0,0 +1,84 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { trpc } from "@/lib/trpc"
+
+const FormSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+    }),
+    password: z.string().min(8, {
+      message: "Password must be at least 8 characters.",
+  })
+})
+export function LoginForm() {
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+      password: ""
+    },
+  })
+
+  const {mutate,data,isSuccess} = trpc.auth.login.useMutation()
+
+  async function onSubmit(values: z.infer<typeof FormSchema>) {
+    mutate(values)
+    isSuccess && data.token && localStorage.setItem('auth',data.token)
+    
+
+}
+
+  return (
+  <Form {...form}>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+      <FormField
+        control={form.control}
+        name="username"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Username</FormLabel>
+            <FormControl>
+              <Input placeholder="shadcn" {...field} />
+            </FormControl>
+            <FormDescription>
+              This is your public display name.
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+       <FormField
+        control={form.control}
+        name="password"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Password</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter your password" {...field} />
+            </FormControl>
+            {/* <FormDescription>
+              This is your public display name.
+            </FormDescription> */}
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <Button type="submit">Submit</Button>
+    </form>
+  </Form>
+)
+}
