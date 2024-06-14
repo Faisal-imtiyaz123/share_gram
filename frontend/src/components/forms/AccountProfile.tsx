40c38d0f138a4ba38441351df1efa304af901e56
Faisal-imtiyaz123 <ee23btech11019@iith.ac.in>
2024-06-14 12:32:37 +0530

    Profile form integration complete

--- frontend/src/components/forms/AccountProfile.tsx
+++ frontend/src/components/forms/AccountProfile.tsx
@@ -1,84 +1,70 @@
 import { Form,FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { UserValidation } from "@/lib/validations/user"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Input } from "@/components/ui/input"
 import { useForm} from "react-hook-form"
 import { Button } from "@/components/ui/button"
 import {z} from "zod"
-import { ChangeEvent, useState } from "react"
-// import { isBase64Image } from "@/lib/utils"
-import { useUploadThing } from "@/lib/uploadthing"
+import { Textarea } from "../ui/textarea"
+import { Cloudinary } from '@cloudinary/url-gen';
+import { auto } from '@cloudinary/url-gen/actions/resize';
+import { autoGravity } from '@cloudinary/url-gen/qualifiers/gravity';
+import CloudinaryUploadWidget from "../uiCustom/Profile/UploadWidget"
+import { useState } from "react"
+import { trpc } from "@/lib/trpc"
+import toast from "react-hot-toast"
+import { useNavigate } from "react-router-dom"
 
 
+
+
+const FormSchema = z.object({
+    name: z.string().min(3, {message: "name must be at least 3 characters."}),
+    bio: z.string().min(20, { message: "Bio must be at least 20 letters."}),
+    username: z.string().min(3, {message: "username must be at least 3 characters."}),
+
+
+})
 
 
 export default function ProfileForm() {
-  const[files,setFiles] = useState<File[]>([])
-  const {startUpload} = useUploadThing("media")
+  const cld = new Cloudinary({cloud: {cloudName: 'dywebzylz'}});
+  const navigate = useNavigate()
+  const mutation = trpc.user.createProfileDetails.useMutation({
+    onError:()=>{
+      toast.error("Profile creation failed")
+    },
+    onSuccess:()=>{
+      toast.success("Profile created successfully"),
+      navigate('/')
+    }
+  })
+  const [UploadedImgUrl, setUploadedImgUrl] = useState<string>("")
+  const img = cld.image('cld-sample-5')
+        .format('auto') // Optimize delivery by resizing and applying auto-format and auto-quality
+        .quality('auto')
+        .resize(auto().gravity(autoGravity()).width(500).height(500)); // Transform the image: auto-crop to square aspect_ratio
+
   const form = useForm({
-    resolver:zodResolver(UserValidation),
+    resolver:zodResolver(FormSchema),
     defaultValues:{
-        profile_photo:"",
         name:"",
         username:"",
         bio:"",
 
     }
   })
-  function handleImage(e:ChangeEvent<HTMLInputElement>,fieldChange:(value:string) =>void){
-    // e.preventDefault()
-    // const fileReader = new FileReader()
-    // if(e.target.files && e.target.files.length>0){
-    //   const file = e.target.files[0]
-    //   setFiles(Array.from(e.target.files))
-    //   if(!file.type.includes("image")) return
-
-    //   fileReader.onload = async (event) =>{
-    //     const imageDataUrl = event.target?.result?.toString() || ""
-    //     fieldChange(imageDataUrl)
-         
-    //   }
-    //   fileReader.readAsDataURL(file)
-
-    // }
-
+  function onSubmit(values:z.infer<typeof FormSchema>){
+    const payload = {...values, profilePicture:UploadedImgUrl}
+    mutation.mutate(payload)
   }
-  async function onSubmit(values:z.infer<typeof UserValidation>){
-    // const blob = values.profile_photo
-    // const hasImageChanged = isBase64Image(blob)
-    // if(hasImageChanged){
-    //   const imgRes = await startUpload(files)
-    //   if(imgRes && imgRes[0].url){
-    //     values.profile_photo = imgRes[0].url
-    //   }
-      
-    }
    
-   
-
-    
-
-  }
    return (
     <Form {...form}>
     <form onSubmit={form.handleSubmit(onSubmit)} className="border-gray-400 border p-4 rounded-md space-y-3 w-[45rem] h-[40rem]">
-      <FormField
-        control={form.control}
-        name="profile_photo"
-        render={({ field }) => (
-          <FormItem>
-            <div className="flex items-center gap-2">
-
-            <FormLabel>
-               </FormLabel>
-            <FormControl>
-              <Input type="file" accept="image/*" placeholder="shadcn" className="cursor-pointer" onChange={(e)=>handleImage(e,field.onChange)} />
-            </FormControl>
-              </div>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
+       <div>
+        <span> Choose Proifle Picture </span>
+        <CloudinaryUploadWidget setUploadedImageUrl={setUploadedImgUrl}/>
+       </div>
        <FormField
         control={form.control}
         name="name"
@@ -116,24 +102,28 @@ export default function ProfileForm() {
         )}
       />
       <FormField
-        control={form.control}
-        name="bio"
-        render={({ field }) => (
-          <FormItem>
-           
-
-            <FormLabel>
-              Bio
-             </FormLabel>
-            <FormControl>
-            </FormControl>
-             
-            <FormMessage />
-          </FormItem>
-        )}
-      />
+          control={form.control}
+          name="bio"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Bio</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Tell us a little bit about yourself"
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                Tell something about you.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
       <div className="flex justify-center">
         <Button className="bg-gray-800 rounded-xl w-full h-14" type="submit">Submit</Button>
+       
 
       </div>
       
