694703f7a5c734e9d64aca2a6d98a8ec95cab6a4
Faisal-imtiyaz123 <ee23btech11019@iith.ac.in>
2024-06-24 08:19:26 +0530

    updated state logic to hold temp users

--- frontend/src/components/uiCustom/Messaging/MessagingSideBar.tsx
+++ frontend/src/components/uiCustom/Messaging/MessagingSideBar.tsx
@@ -1,13 +1,42 @@
 import { trpc } from "@/lib/trpc";
 import SearchedMessagingUserList from "./searchedMessagingUserList";
-import { messageUsers } from "@/lib/types/messageTypes";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { messageUsersDetails } from "@/lib/types/messageTypes";
+import { useParams } from "react-router-dom";
 
 export default function MainMessagingPage() {
-  const {data}:{data:messageUsers[]|undefined} = trpc.message.fetchMessageUsers.useQuery()
-  if(!data) return null
-  
+  const {data,isError,isSuccess,isLoading} = trpc.message.fetchMessageUsers.useQuery()
+  const [messageUsers,setMessageUsers] = useState<messageUsersDetails[]>(isSuccess?data[0].messageUsersDetails:[])
+  const params = useParams()
+  const {data:user} = trpc.user.fetchUser.useQuery({userId:params.id!},{enabled:!!params?.id})
+  console.log(messageUsers)
+  useEffect(()=>{
+    if(isError){
+      toast.error("something went wrong")
+    }
+    if (params.id && user) {
+      setMessageUsers((prev) => {
+        // Check if the user already exists in the state
+        const userExists = prev.some((u) => u._id === user._id.toString());
+        if (userExists) {
+          return prev; // If user exists, return the previous state without changes
+        } else {
+          // If user doesn't exist, add the new user
+          return [...prev, {
+            _id: user._id.toString(),
+            username: user.username,
+            profilePhoto: user.profilePicture,
+            name: user.name
+          }];
+        }
+      });
+    }
+  },[isError,data,params.id,user])
  
-  
+  isLoading && toast.loading('hold on a sec')
+  if(isSuccess){
+    toast.dismiss()
   return (
     <div className="basis-[25%] border-r h-screen">
       <div className="flex flex-col p-4 gap-2">
@@ -23,7 +52,8 @@ export default function MainMessagingPage() {
           }}
         /> */}
       </div>
-      <SearchedMessagingUserList messageUsersArr={data[0]?.messageUsersDetails}/>
+      <SearchedMessagingUserList messageUsersArr={messageUsers}/>
     </div>
   );
+}
 }
