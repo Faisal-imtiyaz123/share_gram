6391c8529913d6fd68a88d231bdc951aca8e4a4a
Faisal-imtiyaz123 <ee23btech11019@iith.ac.in>
2024-06-24 08:20:55 +0530

    updated minor logic

--- frontend/src/components/uiCustom/Search/SearchedUser.tsx
+++ frontend/src/components/uiCustom/Search/SearchedUser.tsx
@@ -10,7 +10,7 @@ export default function SearchedUser({
 }) {
   return (
     <Link
-      className="flex grow h-[5rem] hover:bg-gray-100 p-4 gap-4"
+      className="flex hover:bg-gray-100 p-4 gap-4"
       to={`/home/profile/${user._id.toString()}`}
     >
       <div className="rounded-full  w-[3rem] h-[3rem] relative border border-gray-300 ">
@@ -20,10 +20,9 @@ export default function SearchedUser({
           src={user.profilePicture}
         />
       </div>
- <span className="mt-1 text-gray-800">
-
+   <span className="mt-1 text-gray-800">
       {user.username}
- </span>
+   </span>
     </Link>
   );
 }
