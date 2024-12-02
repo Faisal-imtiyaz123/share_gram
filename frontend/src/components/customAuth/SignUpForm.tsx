23a5ef108de26a954f6a2989c08d781658352023
Faisal-imtiyaz123 <ee23btech11019@iith.ac.in>
2024-12-02 19:56:55 +0530

    fixes password parsing

--- frontend/src/components/customAuth/SignUpForm.tsx
+++ frontend/src/components/customAuth/SignUpForm.tsx
@@ -12,12 +12,12 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 // @ts-expect-error useForm Error
-import {useForm} from "react-hook-form"
+import { useForm } from "react-hook-form"
 import { trpc } from "@/lib/trpc"
 import toast from "react-hot-toast"
 import { useNavigate } from "react-router-dom"
 
-function getPasswordStrength(password:string) {
+function getPasswordStrength(password: string) {
   let strength = 0;
   if (password.length > 5) strength += 1;
   if (password.match(/(?=.*[0-9])/)) strength += 1;
@@ -26,7 +26,8 @@ function getPasswordStrength(password:string) {
   if (password.match(/(?=.*[!@#$%^&*])/)) strength += 1;
   return strength;
 }
-function PasswordStrengthIndicator({ password }:{password:string}) {
+
+function PasswordStrengthIndicator({ password }: { password: string }) {
   const strength = getPasswordStrength(password);
   const color = strength < 2 ? 'bg-red-500' : strength < 4 ? 'bg-yellow-500' : 'bg-green-500';
   const width = `${strength * 20}%`;
@@ -37,34 +38,34 @@ function PasswordStrengthIndicator({ password }:{password:string}) {
     </div>
   );
 }
+
 const formSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
-  password: z.string().min(8).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
+  password: z.string().min(8).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\$!%*?&@])[A-Za-z\d@$!%*?&]{8,}$/, {
     message: "Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character.",
   }),
   confirmPassword: z.string().min(8, { message: "Confirm password must be at least 8 characters" })
 });
 
 export default function SignUpForm() {
-  // const [errors,setErrors]=useState<{path:string,error:string}[]>([])
-  const navigate = useNavigate()
-  const mutatation = trpc.auth.signUp.useMutation({
-    onMutate:()=>{
-      toast.loading("Signing up...")
+  const navigate = useNavigate();
+  const mutation = trpc.auth.signUp.useMutation({
+    onMutate: () => {
+      toast.loading("Signing up...");
     },
-    onError:(error)=>{
-      toast.dismiss()
-      toast.error(`Sign up failed: ${error.message}`)
+    onError: (error) => {
+      toast.dismiss();
+      toast.error(`Sign up failed: ${error.message}`);
     },
-    onSuccess:()=>{
-      toast.dismiss()
-      toast.success("Sign up successful, redirecting to login...")
-      navigate("/login")
-       
+    onSuccess: () => {
+      toast.dismiss();
+      toast.success("Sign up successful, redirecting to login...");
+      navigate("/login");
     }
-  })
+  });
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -72,77 +73,72 @@ export default function SignUpForm() {
       password: "",
       confirmPassword: ""
     },
-  })
-
+  });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
-    if(values.confirmPassword!==values.password) return toast.error("Passwords don't match")
-    mutatation.mutate(values)
+    if (values.confirmPassword !== values.password) return toast.error("Passwords don't match");
+    mutation.mutate(values);
   }
 
   return (
     <div className="flex justify-center h-screen">
-
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-[40vw] mt-[5rem] shadow-lg max-h-[27rem] p-4 border-2 rounded-lg space-y-6">
-        <FormField
-          control={form.control}
-          name="username"
-          // @ts-expect-error useForm Error
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="Enter your username" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-         <FormField
-          control={form.control}
-          name="password"
-          // @ts-expect-error useForm Error
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <div className="flex flex-col gap-4">
-                <Input type="password" placeholder="Enter your password" {...field} />
-                 <PasswordStrengthIndicator password={field.value} />
-                </div>
-              </FormControl>
-              {/* <FormDescription>
-                This is your public display name.
-              </FormDescription> */}
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-         <FormField
-          control={form.control}
-          name="confirmPassword"
-          // @ts-expect-error useForm Error
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Confirm Password</FormLabel>
-              <FormControl>
-                <Input type="password" placeholder="Confirm your password" {...field} />
-              </FormControl>
-              <FormDescription>
-                {/* {errors.includes({path:"confirmPassword"})} */}
-              </FormDescription>
-              <FormMessage>{form.formState.errors.confirmPassword?.message}</FormMessage>
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-
-    </Form>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-[40vw] mt-[5rem] shadow-lg max-h-[27rem] p-4 border-2 rounded-lg space-y-6">
+          <FormField
+            control={form.control}
+            name="username"
+            // @ts-expect-error useForm Error
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter your username" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            // @ts-expect-error useForm Error
+            render={({ field }) => {
+              // Log the password value for debugging
+              console.log("Password:", field.value);
+              return (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <div className="flex flex-col gap-4">
+                      <Input type="password" placeholder="Enter your password" {...field} />
+                      <PasswordStrengthIndicator password={field.value} />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            // @ts-expect-error useForm Error
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirm Password</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="Confirm your password" {...field} />
+                </FormControl>
+                <FormMessage>{form.formState.errors.confirmPassword?.message}</FormMessage>
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
-  )
+  );
 }
